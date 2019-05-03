0f9327bc7585168faa8ede9949a6b867b6612d03
leonard <leonard.devincre@edu.ece.fr>
2019-05-03 13:45:02 +0200

    better creation compte vendeur
    bootstrap

--- vendeur_creation.php
+++ vendeur_creation.php
@@ -6,7 +6,11 @@ session_start();
 <html>
 <head>
     <meta charset="utf-8" />
+    <link href="bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style_register.css" />
+
+
     <link rel="icon" type="image/png" href="images/icone.png" alt="icone Amazon ECE">
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="myscript.js"></script>
@@ -17,25 +21,16 @@ session_start();
     <div id="bloc_page">
 
         <header>
-
             <div id="logo">
                 <a href ="accueil.php"><img src="images/icone.png" alt="Logo Amazon ECE" /></a>
             </div>
-
-
-
             <h1 id="titre_principal">
                 <a href ="accueil.php">Amazon ECE</a>
             </h1>
-
-
-
             <div id="langue">
                 <a href="accueil.php"><img src="images/france.png" alt="langue française" /></a>
                 <a href="#"><img src="images/ru.png" alt="langue anglaise" /></a>
             </div>
-
-
             <nav>
                 <ul>
                     <li><a href="cate.php" id="categories">Catégories</a> <!--menu déroulant-->
@@ -47,7 +42,6 @@ session_start();
                         </ul>
                     </li>
 
-
                     <li><a href="ventes_flash.php">Ventes flash</a></li>
                     <li><a href="votre_compte.php">Votre compte</a></li>
                     <li><a href="vendeur.php">Vendre</a></li>
@@ -55,8 +49,6 @@ session_start();
                     <li><a href="admin.php">Admin</a></li>
                 </ul>
             </nav>
-
-
         </header><br>
 
 
@@ -68,9 +60,63 @@ session_start();
 
         <div id= "corps">
             <div id="ventes_flash">
-
                 <h2>création d'un compte vendeur</h2>
+            </div>
 
+
+
+            <div class="container register">
+                <div class="row">
+                    <div class="col-md-3 register-left">
+                        <img src="images\money.png" alt=""/>
+                        <h3>Bienvenue</h3>
+                        <p>Vous êtes à seulement 30 secondes de pouvoir vendre votre premier article!</p>
+
+                        <input type="submit" name="" value="Connectez-vous!"/><br/>
+                    </div>
+                    <div class="col-md-9 register-right">
+
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                <h3 class="register-heading">Enregistrement vendeur</h3>
+                                <form action="inscription_vendeur.php" method="post">
+                                    <div class="row register-form">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" placeholder="Nom *" value="" name="Nom" />
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" placeholder="Pseudo *" value="" name="Pseudo_Vendeur"/>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control" placeholder="Mot de passe *" value="" name="Mdp" />
+                                            </div>
+                                            <!--<div class="form-group">
+                                                <input type="password" class="form-control"  placeholder="Confirmez votre mot de passe *" value="" name="Mdp"/>
+                                            </div>-->
+
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <input type="email" class="form-control" placeholder="Email *" value="" name="Mail" />
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="file" name="PhotoVendeur" value="fileupload" id="fileupload">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="file" name="ImageFond" value="fileupload" id="fileupload">
+                                            </div>
+                                            <p><font size="2" >Nous ne transmettrons pas vos informations à des tiers</font></p>
+
+
+                                            <input type="submit" class="btnRegister"  value="Enregistrez-vous!"/>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
 
@@ -83,7 +129,19 @@ session_start();
 
 
 
-            <div id="section_votreCompte">
+
+
+
+
+
+
+
+
+
+
+
+
+            <!--<div id="section_votreCompte">
 
 
                 <table frame="box">
@@ -182,7 +240,7 @@ session_start();
                 <br><br>
                 <a href="vendeur.php">Déjà membre ?</a>
 
-            </div>
+            </div>-->
 
         </div>
 
